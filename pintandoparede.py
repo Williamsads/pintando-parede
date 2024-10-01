33c5e4db72c8fd9f27337ec82be9c82e42133115
Seu Nome <seuemail@example.com>
2024-10-01 12:27:39 -0300

    Adicionar cálculo da área da parede e quantidade de tinta necessária para pintura

---
+++ pintandoparede.py
@@ -0,0 +1,11 @@
+largurap= float(input('digite a largura da parede: '))
+alturap= float(input('digite a altura da parede: '))
+
+area= largurap * alturap
+
+tinta= area/2
+
+print('sua parede tem a largura de: {}, a altura de {} e a area e: {}m2'.format(largurap,alturap,area))
+print('para pintar essa parede voce precisa de {}l de tinta'.format(tinta))
+
+
